9cf4bf3da813f4d9c149106284ac6523fa005a55
deepak252 <deepakkudc16102000@gmail.com>
2025-05-22 01:07:15 +0530

    user, todo entity

---
+++ src/database/core.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
